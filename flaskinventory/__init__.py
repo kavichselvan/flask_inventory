686a5e7f75721aebc4ff5cdce39a9d6898e5fc89
Kavichselvan2001@gmail.com <kavichselvan2001@gmail.com>
2024-04-02 13:47:35 +0530

    Updated database credentials

--- flaskinventory/__init__.py
+++ flaskinventory/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '852cbb7ad2de2d36f474737943057604'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rohit:sharma45@localhost/product'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rohit:sharma45@localhost/product?unix_socket=/var/run/mysqld/mysqld.sock'
 db = SQLAlchemy(app)
 
 from flaskinventory import routes
